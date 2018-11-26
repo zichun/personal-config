786086eb35f4b26c743e8cb077b5e7fa00d3a88d
zichun <zichun@gmail.com>
2018-11-26 11:07:30 -0800

    Update install.ps1 - fix comments

--- install.ps1
+++ install.ps1
@@ -17,19 +17,19 @@ mkdir \temp;
 cd \temp;
 & "$($env:ProgramFiles)\Git\cmd\git" clone https://github.com/zichun/personal-config.git;
 
-; Emacs configs
+# Emacs configs
 cd \temp\personal-config\emacs;
 Copy-Item -Force -Recurse * $env:USERPROFILE;
 New-Item -Path env:\HOME -Value $env:USERPROFILE;
 
-; Powershell configs
+# Powershell configs
 cd \temp\personal-config\powershell;
 copy * "$($env:USERPROFILE)\Documents\WindowsPowerShell\";
 
-; ripgrep config
+# ripgrep config
 cd \temp\personal-config\ripgrep;
 copy * $env:USERPROFILE;
 
-; cmder config
+# cmder config
 cd \temp\personal-config\cmder;
 copy * \tools\Cmder\vendor\conemu-maximus5\;
